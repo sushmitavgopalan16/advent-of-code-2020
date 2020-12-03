28bc396998a06364ee22b7875353945e27ec683c
Sushmita V Gopalan <sushmitavgopalan@gmail.com>
2020-12-02 23:30:24 -0600

    spaces

--- day-three/day-three.py
+++ day-three/day-three.py
@@ -4,13 +4,12 @@ with open('day3.txt') as f:
     l = f.read().splitlines()
 
 def get_answer(l,row_step, col_step):
-    k = [k*3200 for k in l]
+    k = [k * 3200 for k in l]
 
     trees = 0
     row = 0
     col = 0
     for line in k:
-        #print(line)
         row = row + row_step
         col = col + col_step
         try:
@@ -26,4 +25,4 @@ def get_answer(l,row_step, col_step):
 get_answer(l,1,3)
 
 # question 2
-get_answer(l,1,1)*get_answer(l,1,3)*get_answer(l,1,5)*get_answer(l,1,7)*get_answer(l,2,1)
+get_answer(l,1,1) * get_answer(l,1,3) * get_answer(l,1,5) * get_answer(l,1,7) * get_answer(l,2,1)
